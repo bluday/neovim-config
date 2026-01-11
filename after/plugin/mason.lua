a89ba5cc900690449e9b6b171b55ff58eb0c806a
BluDay <19403893+bluday@users.noreply.github.com>
2026-01-11 13:39:22 +0100

    feat(mason): specify language servers to use

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,1 +1,12 @@
 require("mason").setup()
+
+require("mason-lspconfig").setup {
+    ensure_installed = {
+        "asm_lsp",
+        "clangd",
+        "lua_ls",
+        "omnisharp",
+        "pylsp",
+        "rust_analyzer"
+    }
+}
